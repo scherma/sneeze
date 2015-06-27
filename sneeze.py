1e110820c9027fd78afb6c75d8212d3af33808fa
scherma <joash.lewis@gmail.com>
2015-06-27 01:34:09 +0100

    Initial files

---
+++ sneeze.py
@@ -0,0 +1,33 @@
+#!/usr/local/bin/python
+
+import time
+from watchdog.observers import Observer
+import os
+from hayfever import HayFever
+import sys
+			
+
+if __name__ == "__main__":
+	d = {}
+	with open("sneeze.conf") as f:
+    		for line in f:
+       			(key, val) = line.strip().split(": ")
+			d[key] = val
+	if not os.path.isdir(d['watch']):
+		raise ValueError("{} is not a valid directory.".format(sys.argv[1]))
+	if len(d['destination']) < 1:
+		raise ValueError("Destination must be provided.")
+
+
+	# create event handler
+    	event_handler = HayFever(**d)
+	# create event detector
+    	observer = Observer()
+    	observer.schedule(event_handler, d['watch'], recursive=True)
+    	observer.start()
+    	try:
+        	while True:
+            		time.sleep(1)
+    	except KeyboardInterrupt:
+        	observer.stop()
+	observer.join()
